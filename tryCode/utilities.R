2556c407f6668059ebb36ecbb05bd0ab79c3afcb
JINhuaqing <kevinjin0423@gmail.com>
2020-06-18 11:02:16 +0800

    Add utitilities.R file and add new function for latex table
    Changes to be committed:
    	modified:   tryCode/utilities.R

--- tryCode/utilities.R
+++ tryCode/utilities.R
@@ -1,6 +1,6 @@
 library(magrittr)
 # return latex scr code for output
-latex.out.fn <- function(res, prefix, tidx){
+latex.out.fn <- function(res, prefix, tidx, suffix=NULL){
   MTDs <- res$MTDs.percent  
   MTDs <- round(MTDs*100, 1)
   if (!missing(tidx)){
@@ -10,13 +10,14 @@ latex.out.fn <- function(res, prefix, tidx){
   tss <- res$t.dose
   tdlt <- res$t.DLT
   row1 <- paste("&", MTDs, collapse=" ") 
-  row1 <- paste(prefix, "& MTD %", row1); 
+  row1 <- paste(prefix, "& MTD %", row1, "\\\\"); 
   
   row2 <- paste("&", round(av.dose, 1), collapse=" ") 
-  row2 <- paste("& ASN", row2, "&", round(tdlt, 1), "&", round(tss, 1))
-  print(row1)
-  print(row2)
+  row2 <- paste("& ASN", row2, "&", round(tdlt, 1), "&", round(tss, 1), "\\\\")
+  out <- paste(row1, "\n", row2, suffix)
+  cat(out)
 }
 
 
-latex.out.fn(res, prefix="m=1", 1)
+#latex.out.fn(res, prefix="m=1", 1, "[1pt]")
+
