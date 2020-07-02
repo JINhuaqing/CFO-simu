b269b38e720330d5427fb4fdb4bde64d28fe0d93
JINhuaqing <kevinjin0423@gmail.com>
2020-07-02 16:03:50 +0800

    minor

--- Rcode/res_plot.R
+++ Rcode/res_plot.R
@@ -3,10 +3,11 @@ setwd("C:/Users/Dell/Documents/ProjectCode/phaseI")
 source("Rcode/utilities.R")
 
 
-load("results/Allmethods10000TSS_30_Level_5.RData")
+load("results/Simu10000Level_3_07.RData")
 res <- post.process.random(results)
 res <- res[c(1, 2, 6, 7), ]
 res
 M.names <- c("Butterfly-BF", "Butterfly-Odds", "CRM", "BOIN")
 res.ggplot.fn(res, filename="results/Level5.jpg", main="Cohortsize=3, ncohort=10", M.names=M.names, angle=45)
+
 
