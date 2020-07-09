8c22d5161d303a130e36c1e720498f32030ac582
JinHuaqing <jinhuaqing@hku.hk>
2020-07-09 13:21:56 +0800

    要提交的变更：    （使用 "git reset HEAD <file>..." 撤出暂存区）
    	修改：      Rcode/butterfly_utils.R

--- Rcode/butterfly_utils.R
+++ Rcode/butterfly_utils.R
@@ -667,3 +667,163 @@ post.process.raw <- function(ress){
     
     list(MTDs.percent=MTDs.percent, av.dose=dose.mean, av.DLT=DLTs.mean, t.dose=sum(dose.mean), t.DLT=sum(DLTs.mean), p.true=res$p.true, target=res$target)
 }
+
+
+# R code Related to BMS method
+
+sample.ML <- function(phi, up.lim, N=10000){
+    # phi: Target toxicity
+    # N : the number of samples
+    if (missing(up.lim)){
+        up.lim <- 0.6
+    }
+    sps.all <- runif(2*N, min=phi, max=up.lim)
+    sps.all <- matrix(sps.all, ncol=2)
+    reorder.sps <- t(apply(sps.all, 1, sort))
+    p2.sps <- reorder.sps[, 1]
+    p3.sps <- reorder.sps[, 2]
+    list(p2.sps=p2.sps, p3.sps=p3.sps)
+}
+
+sample.MC <- function(phi, up.lim, N=10000){
+    # phi: Target toxicity
+    # N : the number of samples
+    if (missing(up.lim)){
+        up.lim <- 0.6
+    }
+    
+    p1.sps <- runif(N, min=0, max=phi)
+    p3.sps <- runif(N, min=phi, max=up.lim)
+    list(p1.sps=p1.sps, p3.sps=p3.sps)
+}
+
+
+sample.MR <- function(phi, N=10000){
+    # phi: Target toxicity
+    # N : the number of samples
+    sps.all <- runif(2*N, min=0, max=phi)
+    sps.all <- matrix(sps.all, ncol=2)
+    reorder.sps <- t(apply(sps.all, 1, sort))
+    p1.sps <- reorder.sps[, 1]
+    p2.sps <- reorder.sps[, 2]
+    list(p1.sps=p1.sps, p2.sps=p2.sps)
+}
+
+
+BMS.prob3 <- function(phi, cys, cns, model="L"){
+    yL <- cys[1]
+    yC <- cys[2]
+    yR <- cys[3]
+    
+    nL <- cns[1]
+    nC <- cns[2]
+    nR <- cns[3]
+    
+    if (model=="L"){
+        sps <- sample.ML(phi) 
+        p2.sps <- sps$p2.sps
+        p3.sps <- sps$p3.sps
+        itm1 <- phi**yL * (1-phi)**(nL-yL)
+        itm2.all <- p2.sps**yC*(1-p2.sps)**(nC-yC) * p3.sps**yR*(1-p3.sps)**(nR-yR)
+        itm2 <- mean(itm2.all)
+        return(itm1*itm2)
+    }else if (model=="C"){
+        sps <- sample.MC(phi) 
+        p1.sps <- sps$p1.sps
+        p3.sps <- sps$p3.sps
+        itm1 <- phi**yC * (1-phi)**(nC-yC)
+        itm2.all <- p1.sps**yL*(1-p1.sps)**(nL-yL) * p3.sps**yR*(1-p3.sps)**(nR-yR)
+        itm2 <- mean(itm2.all)
+        return(itm1*itm2)
+    }else if (model=="R"){
+        sps <- sample.MR(phi) 
+        p1.sps <- sps$p1.sps
+        p2.sps <- sps$p2.sps
+        itm1 <- phi**yR * (1-phi)**(nR-yR)
+        itm2.all <- p1.sps**yL*(1-p1.sps)**(nL-yL) * p2.sps**yC*(1-p2.sps)**(nC-yC)
+        itm2 <- mean(itm2.all)
+        return(itm1*itm2)
+    }
+    
+}
+
+BMS.prob2L <- function(phi, cys, cns, model="L"){
+    yL <- cys[1]
+    yC <- cys[2]
+    
+    nL <- cns[1]
+    nC <- cns[2]
+    
+    if (model=="L"){
+        p2.sps <- runif(10000, min=phi, max=0.6)
+        itm1 <- phi**yL * (1-phi)**(nL-yL)
+        itm2.all <- p2.sps**yC*(1-p2.sps)**(nC-yC)
+        itm2 <- mean(itm2.all)
+        return(itm1*itm2)
+    }else if (model=="C"){
+        p1.sps <- runif(10000, min=0, max=phi)
+        itm1 <- phi**yC * (1-phi)**(nC-yC)
+        itm2.all <- p1.sps**yL*(1-p1.sps)**(nL-yL)
+        itm2 <- mean(itm2.all)
+        return(itm1*itm2)
+    }
+}
+
+
+BMS.prob2R <- function(phi, cys, cns, model="C"){
+    yC <- cys[2]
+    yR <- cys[3]
+    
+    nC <- cns[2]
+    nR <- cns[3]
+    
+    if (model=="C"){
+        p3.sps <- runif(10000, min=phi, max=0.6)
+        itm1 <- phi**yC * (1-phi)**(nC-yC)
+        itm2.all <- p3.sps**yR*(1-p3.sps)**(nR-yR)
+        itm2 <- mean(itm2.all)
+        return(itm1*itm2)
+    }else if (model=="R"){
+        p2.sps <- runif(10000, min=0, max=phi)
+        itm1 <- phi**yR * (1-phi)**(nR-yR)
+        itm2.all <- p1.sps**yL*(1-p1.sps)**(nL-yL) * p2.sps**yC*(1-p2.sps)**(nC-yC)
+        itm2 <- mean(itm2.all)
+        return(itm1*itm2)
+    }
+    
+}
+
+BMS.model.prob <- function(phi, cys, cns, cover.doses){
+    if (cover.doses[2]==1){
+        ps <- c(1, 0, 0)
+    }else{
+        if (is.na(cys[1]) & (cover.doses[3]==1)){
+            ps <- c(0, 1, 0)
+        }
+        
+        if (is.na(cys[1]) & (!(cover.doses[3]==1))){
+            pC <- BMS.prob2R(phi, cys, cns, "C")    
+            pR <- BMS.prob2R(phi, cys, cns, "R")    
+            ups <- c(0, pC, pR)
+            ps <- ups/sum(ups)
+        }
+        
+        if (is.na(cys[3]) | (cover.doses[3]==1)){
+            pL <- BMS.prob2L(phi, cys, cns, "L")    
+            pC <- BMS.prob2L(phi, cys, cns, "C")    
+            ups <- c(pL, pC, 0)
+            ps <- ups/sum(ups)
+        }
+        
+        if (!(is.na(cys[1]) | is.na(cys[3]) | cover.doses[3]==1)){
+            pL <- BMS.prob3(phi, cys, cns, "L")    
+            pC <- BMS.prob3(phi, cys, cns, "C")    
+            pR <- BMS.prob3(phi, cys, cns, "R")    
+            ups <- c(pL, pC, pR)
+            ps <- ups/sum(ups)
+        }
+        
+    }
+    names(ps) <- c("D prob", "S prob", "E prob")
+    ps
+}
