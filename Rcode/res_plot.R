c1a1b0ce10ef3fb7a4eab65d2801b355faf68bb4
JINhuaqing <kevinjin0423@gmail.com>
2020-07-09 12:00:45 +0800

    On branch preana  Changes to be committed: 	new file:   Rcode/BMS_debug.R 	modified:   Rcode/res_plot.R

--- Rcode/res_plot.R
+++ Rcode/res_plot.R
@@ -3,11 +3,10 @@ setwd("C:/Users/Dell/Documents/ProjectCode/phaseI")
 source("Rcode/utilities.R")
 
 
-load("results/Simu10000Level_3_07.RData")
+load("results/Simu10000Level_7_15.RData")
 res <- post.process.random(results)
-res <- res[c(1, 2, 6, 7), ]
 res
 M.names <- c("Butterfly-BF", "Butterfly-Odds", "CRM", "BOIN")
-res.ggplot.fn(res, filename="results/Level5.jpg", main="Cohortsize=3, ncohort=10", M.names=M.names, angle=45)
+res.ggplot.fn(res, filename="results/Level7_15.jpg", main="Cohortsize=3, ncohort=10, 7 Levels, 0.15", M.names=M.names, angle=45)
 
 
