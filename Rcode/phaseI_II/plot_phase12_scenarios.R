1f7fc0fa809890321ef162db0f8d7a5cf3f8fd9e
JINhuaqing <kevinjin0423@gmail.com>
2020-11-18 20:54:02 +0800

    ...

--- Rcode/phaseI_II/plot_phase12_scenarios.R
+++ Rcode/phaseI_II/plot_phase12_scenarios.R
@@ -1,32 +1,62 @@
-phaseI_II.scenario.plot.fn <- function(p.true, pE.true, main="Plateau"){
+setwd("C:/Users/Dell/Documents/ProjectCode/phaseI")
+phaseI_II.scenario.plot.fn <- function(p.true, pE.true, main="Plateau", OBD=NULL){
     ndose <- length(p.true)
+    lvls <- 1:ndose
     cols <- c("black", "red")
     lwds <- c(2, 2)
     ltys <- c(1, 2)
     pchs <- c(15, 16)
-    plot(1:ndose, p.true, ylab = "Probability", xlab="Dose level", main=main, 
+    plot(1:ndose, p.true, ylab = "Probability", xlab="Dose level", ylim=c(0, 0.8), main=main, xaxt="n", 
          type="b", 
          lwd=lwds[1], col=cols[1], lty=ltys[1], pch=pchs[1])
+    if (is.null(OBD)){
+        axis(1, at=lvls, lvls)
+    }else{
+        axis(1, at=OBD, paste0(OBD, "*"), col.axis="red", font.axis=2)
+        axis(1, at=lvls[-OBD], lvls[-OBD])
+    }
     lines(1:ndose, pE.true, type="b", lwd=lwds[2], col=cols[2], lty=ltys[2], pch=pchs[2])
 #    legend("topleft", c("DLT rate", "Efficacy rate"), col=cols, lty=ltys, lwd=lwds, pch=pchs)
 }
 
 
-p.true <- c(0.1, 0.2, 0.3, 0.4, 0.5)
-pE.true <- c(0.1, 0.2, 0.3, 0.2, 0.1)
+scenarios <- list()
+scenarios[[1]] <- list(p.true=c(0.05, 0.1, 0.3, 0.5, 0.6),
+                      pE.true=c(0.2, 0.3, 0.5, 0.5, 0.5))
+scenarios[[2]] <- list(p.true=c(0.15, 0.25, 0.3, 0.35, 0.4),
+                      pE.true=c(0.2, 0.5, 0.5, 0.5, 0.5))
 
-dev.off()
+scenarios[[3]] <- list(p.true=c(0.10, 0.22, 0.25, 0.3, 0.4),
+                       pE.true=c(0.3, 0.6, 0.55, 0.35, 0.2))
+scenarios[[4]] <- list(p.true=c(0.05, 0.15, 0.25, 0.40, 0.45),
+                        pE.true=c(0.08, 0.17, 0.45, 0.30, 0.25))
+
+scenarios[[5]] <- list(p.true=c(0.05, 0.07, 0.1, 0.12, 0.16),
+                     pE.true=c(0.35, 0.45, 0.5, 0.55, 0.75))
+
+scenarios[[6]] <- list(p.true=c(0.25, 0.40, 0.5, 0.55, 0.6),
+                       pE.true=c(0.15, 0.25, 0.5, 0.5, 0.5))
+for (i in 1:6){
+    sce <- scenarios[[i]]
+    outstr <- paste0("$(", sce$p.true, ",", " ", sce$pE.true, ")$")
+    outstr <- paste(outstr, collapse = " & ")
+    outstr <- paste0("&", outstr, " \\\\", "\n")
+    cat(outstr)
+}
+
+
+#jpeg("plots/eff_tox_sc6.jpg", width=480*1.2, height=720*1.2, pointsize=15)
 de.par <- par()
 mat <- matrix(c(1, 2, 3, 4, 5, 6, 7, 7), nrow=4, byrow=TRUE)
 layout(mat=mat, heights=c(0.5, 0.5, 0.5, 0.2))
 par(mar=de.par$mar)
 
-phaseI_II.scenario.plot.fn(p.true, pE.true)
-phaseI_II.scenario.plot.fn(p.true, pE.true)
-phaseI_II.scenario.plot.fn(p.true, pE.true)
-phaseI_II.scenario.plot.fn(p.true, pE.true)
-phaseI_II.scenario.plot.fn(p.true, pE.true)
-phaseI_II.scenario.plot.fn(p.true, pE.true)
+phaseI_II.scenario.plot.fn(scenarios[[1]]$p.true, scenarios[[1]]$pE.true, main="scenario 1", OBD=3)
+phaseI_II.scenario.plot.fn(scenarios[[2]]$p.true, scenarios[[2]]$pE.true, main="scenario 2", OBD=2)
+phaseI_II.scenario.plot.fn(scenarios[[3]]$p.true, scenarios[[3]]$pE.true, main="scenario 3", OBD=2)
+phaseI_II.scenario.plot.fn(scenarios[[4]]$p.true, scenarios[[4]]$pE.true, main="scenario 4", OBD=3)
+phaseI_II.scenario.plot.fn(scenarios[[5]]$p.true, scenarios[[5]]$pE.true, main="scenario 5", OBD=5)
+phaseI_II.scenario.plot.fn(scenarios[[6]]$p.true, scenarios[[6]]$pE.true, main="scenario 6", OBD=NULL)
 
 par(mar = c(0,2,0,2))
 plot(1, type = "n", axes=FALSE, xlab="", ylab="")
@@ -35,3 +65,4 @@ lwds <- c(2, 2)
 ltys <- c(1, 2)
 pchs <- c(15, 16)
 legend(x="top", inset=0, legend=c("DLT rate", "Efficacy rate"), col=cols, lty=ltys, lwd=lwds, pch=pchs, horiz=TRUE)
+dev.off()
--- results/res.R
+++ results/res.R
@@ -1,13 +1,14 @@
+setwd("C:/Users/Dell/Documents/ProjectCode/phaseI/results")
 source("../Rcode/utilities.R")
-# plateau: 2, 4
-# umbrella: 7, 12, 11
-# increasing: 1, 4
-# on OBD: 2
-idx <- 4
+# plateau: 2, 4 / 2, 4
+# umbrella: 7, 12, 11 / 7, 12
+# increasing: 1, 4 / 1
+# on OBD: 2 / 2
+idx <- 2
 fName <- "plateau"
 fName <- "umbrella"
 fName <- "increa"
-#fName <- "noOBD"
+fName <- "noOBD"
 
 fName.all <- paste0("res_", fName, "_1000_", idx, ".RData")
 fName.efftox <- paste0("efftox_res_", fName, "_1000_", idx, ".RData")
@@ -17,5 +18,29 @@ fName.efftox <- paste0("efftox_res_", fName, "_1000_", idx, ".RData")
 load(fName.efftox)
 load(fName.all)
 sum.all[["efftox"]] <- sum.res.efftox
-#ls()
-phase.I.II.pretty.tb(sum.all)
+res <- phase.I.II.pretty.tb(sum.all);res
+
+nams <- c("STEIN", "CF", "MADA", "EffTox")
+#nams <- c("STEIN", "CF", "CF.alter", "MADA", "EffTox")
+res.trans <- data.frame(lapply(res, as.character), stringsAsFactors = FALSE)
+row.names(res.trans) <- nams
+res.trans <- res.trans[, -8]
+
+
+trans.f <- function(eachRow){
+    level5 <- sapply(eachRow[1:5], function(i)paste(strsplit(i, "(", fixed=TRUE)[[1]], collapse = " ("))
+    itm1 <- paste(level5, "& ")
+    itm1 <- paste(itm1, collapse = "")
+    itm2 <- paste0(eachRow[6], "/", eachRow[7])
+    res.str <- paste(itm1, itm2, "&", eachRow[8])
+    res.str <- paste(row.names(eachRow), "&", res.str, "\\\\")
+    res.str
+}
+
+for (i in c(2, 4, 3, 1)){
+#for (i in c(2, 5, 4, 1)){
+    cat(trans.f(res.trans[i, ]), "\n")
+}
+
+
+
