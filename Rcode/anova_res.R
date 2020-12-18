9bab8affe301c78be6c1527b624104f7f5895cb3
Kevin Jin <kevinjin0423@gmail.com>
2020-12-18 16:22:40 +0800

    add plots for gamma

--- Rcode/anova_res.R
+++ Rcode/anova_res.R
@@ -1,13 +1,23 @@
+#library(bestNormalize)
 library(dplyr)
 setwd("C:/Users/Dell/Documents/ProjectCode/CFO")
-setwd("C:/Users/JINHU/Documents/ProjectCode/phaseI")
+setwd("C:/Users/JINHU/Documents/ProjectCode/CFO")
 source("./Rcode/utilities.R")
 source("./Rcode/phaseI/anova_settings.R")
 anova.ress <- dir("./results/Anovas", pattern="*.RData", full.names = TRUE)
 
 sep.paths <- strsplit(anova.ress, "_")
 raw.labs <- sapply(sep.paths, function(i)i[3])
-labs <- lapply(strsplit(raw.labs, ""), function(i)as.numeric(i))
+tmp.fn <- function(i){
+    if (length(i)==5){
+        return(as.numeric(i))
+    }else{
+       num.i <- as.numeric(i) 
+       rv <- c(num.i[1:2], 10*num.i[3]+num.i[4], num.i[5:6])
+       return(rv)
+    }
+}
+labs <- lapply(strsplit(raw.labs, ""), tmp.fn)
 mat.labs <- do.call(rbind, labs)
 
 anova.data.list <- list()
@@ -45,13 +55,31 @@ anova.data.df$SampleSizes <- as.factor(anova.data.df$SampleSizes)
 anova.data.df$DiffProbs <- as.factor(anova.data.df$DiffProbs)
 anova.data.df$Targets <- as.factor(anova.data.df$Targets)
 
-
-fit <- aov(MTD.Sel~nLevels+CohortSize+SampleSizes+DiffProbs+Targets+Methods, data=anova.data.df)
+anova.data.df <- filter(anova.data.df, CohortSize==1|CohortSize==3,
+                        SampleSizes==21|SampleSizes==30|SampleSizes==48|SampleSizes==60,
+                        DiffProbs!=0.15)
+fit <- aov(MTD.Sel~(nLevels+CohortSize+SampleSizes+DiffProbs+Targets+Methods)*(nLevels+CohortSize+SampleSizes+DiffProbs+Targets+Methods), data=anova.data.df)
+#fit <- aov(MTD.Sel~nLevels+CohortSize+SampleSizes+DiffProbs+Targets+Methods, data=anova.data.df)
 summary(fit)
+# main factors, DiffProbs, nLevels, SampleSize
 
-# Sample sizes 30, 60
+# fit diagnostic
+errs <- fit$residuals
+norm.errs <- errs/sd(errs)
+ks.test(norm.errs, pnorm)
+#plot(norm.errs)
+#norm.errs %>% density %>% plot
+#rnorm(10000) %>% density %>% lines(col="red")
+
+fit.method <- aov(MTD.Allo~Methods, data=anova.data.df)
+summary(fit.method)
+
+
+
 dat <- group_by(anova.data.df, Methods)
 dat <- group_by(anova.data.df, SampleSizes, Methods)
+dat <- group_by(anova.data.df, DiffProbs, Methods)
+dat <- group_by(anova.data.df, nLevels, Methods)
 summarise(dat, MTD.Sel=mean(MTD.Sel), MTD.Allo=mean(MTD.Allo))
 summarise(dat, Over.Sel=mean(Over.Sel), Over.Allo=mean(Over.Allo), 
           Risk.of.HT=mean(Risk.of.HT), PerDLT=mean(PerDLT))
@@ -60,8 +88,14 @@ summarise(dat, Over.Sel=mean(Over.Sel), Over.Allo=mean(Over.Allo),
 # Targets is less important
 # nlevel has some effect
 # DiffProbs
-#sample sizes 30, 42
+#sample sizes 21, 30, 48, 60
 sub.data <- filter(anova.data.df, 
-                   SampleSizes==48|SampleSizes==30|SampleSizes==42, CohortSize==1|CohortSize==3)
+                   SampleSizes==42)
 sub.data %>% group_by(Methods) %>%
         summarise(MTD.Sel=mean(MTD.Sel), MTD.Allo=mean(MTD.Allo))
+
+MTD.Sel <- anova.data.df$MTD.Sel
+SST <- sum((MTD.Sel - mean(MTD.Sel))**2)
+sum.fit <- summary(fit)
+SSE.main <- sum(sum.fit[[1]]$`Sum Sq`[c(1, 3, 4)])
+SSE.main/SST
